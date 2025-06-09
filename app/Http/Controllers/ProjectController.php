b580a6cd4046cc3631b10397d16f78b5697cc137
RaveeshaPeiris <rpeiris4444@gmail.com>
2025-06-09 11:50:48 +0530

    location

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    
+    public function getLocation($id)
+    {
+        $project = Project::find($id);
+
+        if ($project) {
+            return response()->json([
+                
+                'lattitude' => $project->lattitude, 
+                'longitude' => $project->longitude,
+            ]);
+        }
+
+        return response()->json(['error' => 'Project not found'], 404);
+    }
 }
